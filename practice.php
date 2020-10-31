1a179bf172ce5765d84c5b06426f249d14eabe13
jun <redwings.1.800.359@gmail.com>
2020-10-31 09:26:10 +0000

    課題提出

---
+++ practice.php
@@ -0,0 +1,24 @@
+<?php
+echo "hello php!";
+echo "\n";
+
+$a = 3;
+$b = 7;
+echo $a + $b;
+echo "\n";
+
+echo "課題2 : "; 
+$array_mouth =["january","febrary","march","april","may","june","july","august","september","october","november","december"]; 
+echo $array_mouth[7];
+echo "\n"; 
+
+$hello = "Hello,";
+$name = "jun'";
+$worid = "s World!";
+echo $hello . $name . $worid;
+echo "\n";
+
+$tech_boost = "tech";
+$tech_boost .= "boost";
+echo $tech_boost;
+echo "\n";
