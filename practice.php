abf28c38151e02b13a36038cff0394707efefa85
jun <redwings.1.800.359@gmail.com>
2020-11-04 12:58:58 +0000

    課題提出3章

--- practice.php
+++ practice.php
@@ -22,3 +22,116 @@ $tech_boost = "tech";
 $tech_boost .= "boost";
 echo $tech_boost;
 echo "\n";
+
+$value = 6;
+echo $value + 2;
+echo"\n";
+
+$integer = 20;
+$strigs = "abc";
+echo $integer;
+echo $strigs;
+echo "\n";
+
+$new_integer = $integer + 10;
+echo $new_integer;
+echo"\n";
+
+$array = [2017,2018,2019,2020];
+echo $array[0];
+echo $array[2];
+echo"\n";
+
+$array2 = ["spring","summer","autumn","winter"];
+echo $array2[2];
+echo"\n";
+
+$animals = [
+  "cat" => "猫",
+  "dog" => "犬",
+  "bird" => "鳥"
+  ]; 
+
+  echo $animals["dog"];
+  echo"\n";
+  
+  $result = true ;
+  if ($result == true) {
+    echo "成功しました";
+  } else {
+    echo"失敗しました";
+  }
+  echo"\n";
+  
+  $value = 6;
+  echo $value + 2;
+  echo"\n";
+  
+  $value = 6;
+  echo $value - 2;
+  echo"\n";
+  
+  $value = 6;
+  echo $value *2;
+  echo"\n";
+  
+ $value = "aaa";
+ $value2 = "bbb";
+ echo $value . "bbb";
+ echo"\n";
+
+$value = 10;
+$result = $value == 20;
+var_dump($result);
+echo"\n";
+
+$value = 10;
+$result = $value < 20;
+var_dump($result);
+echo"\n";
+
+$value = 10;
+$result = $value > 20;
+var_dump($result);
+echo"\n";
+
+$a = "20";
+$b = 20;
+$result = $a == $b;
+var_dump($result);
+echo"\n";
+$result = $a === $b;
+var_dump($result);
+echo"\n";
+
+$value = 10;
+++$value;
+echo $value;
+echo"\n";
+
+$value = 10;
+--$value;
+echo $value;
+echo"\n";
+
+$value = 10;
+$value += 5;
+echo $value;
+echo"\n";
+
+$value = 10;
+$value -= 5;
+echo $value;
+echo"\n";
+
+$value = "apple";
+$value .= "orange";
+echo $value;
+echo"\n";
+
+$value = 10;
+$result = ($value < 20) ? '$valueは20より小さい' :
+'$valueは20と等しいかまたは大きい';
+echo $result;
+echo"\n";
+
---
+++ practice2.php
@@ -0,0 +1,105 @@
+<?php
+$height = 160;
+if ($height < 150){
+echo "150ｃｍ未満の方はご乗車できません";
+} else {
+echo "ご乗車になれます";
+}
+echo"\n";
+
+$height = 230;
+if ($height < 150) {
+  echo"150cm未満の方はご乗車できません。";
+  }else if ($height >= 200){
+  echo"200cm以上の方はご乗車できません。";
+  } else {
+  echo"ご乗車になれます";  
+  } 
+  echo"\n";
+  
+  $weekday = "月曜";
+  switch ($weekday) {
+    case "月曜":
+    echo "可燃ごみの日です。";
+    break;
+    case"水曜":
+    echo "資源ごみの日です。";
+    break;
+    default:
+    echo"回収はありません。";
+    break;
+    echo"\n";
+  }
+  
+  $weekday = "木曜";
+  switch($weekday) {
+  case"月曜":
+  case"木曜":
+  echo"可燃ごみの日です。";
+  break;
+  case"水曜":
+  echo"資源ごみの日です。";
+  break;
+  default:
+  echo"回収はありません。";
+  break;
+  }
+  echo"\n";
+  
+  $a = 3;
+  $b = 3;
+  $c = "3";
+  
+  var_dump($a == $b);
+  var_dump($a !=  $b);
+  var_dump($a > $b);
+  
+  for($i = 0; $i < 10; $i++){
+    echo $i;
+  }
+  echo"\n";
+  
+$total =0;
+echo $total;
+
+for ($i = 0; $i <= 100; $i++) {
+    $total += $i;
+}
+echo $total;
+
+$fruits = array("apple","orange","lemon",);
+echo count($fruits);
+echo"\n";
+
+for($i = 0; $i <count($fruits); $i++) {
+  echo"要素は" . $fruits[$i];
+  echo"\n";
+}
+
+$animals = array("dog","cat","panda");
+foreach($animals as $animals){
+  echo"要素は" . $animals;
+  echo"\n";
+}
+
+$name = "jun";
+if($name == "taro") {
+  echo "私は $name です";
+} else{
+  echo "あなたの名前ではありません";
+}
+echo"\n";
+$total = 0;
+for($i = 0;$i <= 10000; $i++ ){
+  $total += $i;
+}
+echo $total;
+echo"\n";
+
+$fruits = ["apple", "orange", "banana", "grape", "melon"];
+foreach ($fruits as $fruit) {
+  echo  $fruit;
+  echo"\n";
+}
+  
+
