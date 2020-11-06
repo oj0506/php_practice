440edda8a8f6f8e9f0acecc015435211d461550b
jun <redwings.1.800.359@gmail.com>
2020-11-06 10:10:28 +0000

    応用課題提出2

--- practice2.php
+++ practice2.php
@@ -90,7 +90,7 @@ if($name == "taro") {
 }
 echo"\n";
 $total = 0;
-for($i = 0;$i <= 10000; $i++ ){
+for($i = 1;$i <= 10000; $i++ ){
   $total += $i;
 }
 echo $total;
@@ -101,5 +101,17 @@ foreach ($fruits as $fruit) {
   echo  $fruit;
   echo"\n";
 }
-  
+/* for文の始めの値を定義する */
+  $start = 1;
+/* for文の終わりの値を定義する */
+$end = 100;
+
+for($i = $start; $i <= $end; $i++){
+
+  // 5で割り切れたら{}内を実行する
+  if($i % 5 == 0){
+    echo $i;
+  } echo"\n";
+}  
+
 
